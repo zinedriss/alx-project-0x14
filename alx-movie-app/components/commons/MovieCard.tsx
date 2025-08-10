e53e0cbfdbd96af75fd6601bcd89800bf75ba2e3
idriss <ssenuoy.issiurd@gmail.com>
2025-08-10 19:38:05 +0100

    make some heading pages

--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import { MovieProps } from "@/interfaces"
+import Image from "next/image"
 
-const MovieCard: React.FC = () => {
+const MovieCard: React.FC<MovieProps> = ({ title, posterImage, releaseYear }) => {
   return (
-    <div>
-      MovieCard Component
-    </div>
-  );
-};
+    <div className="h-[563px]">
+      <div>
+        <Image className="h-[430px] w-full rounded-md hover:cursor-pointer" src={posterImage} width={100} height={100} alt={title} />
 
-export default MovieCard;
+      </div>
+      <div className="flex justify-between py-4">
+        <p className="text-xl font-bold">{title}</p>
+        <p className="text-xl text-[#E2D609]">{releaseYear}</p>
+      </div>
+    </div>
+  )
+}
+
+export default MovieCard
--- alx-movie-app/interfaces/index.ts
+++ alx-movie-app/interfaces/index.ts
@@ -8,3 +8,29 @@ export interface ButtonProps {
   title: string
   action?: () => void
 }
+
+export interface MovieProps {
+  id?: string
+  posterImage: string
+  releaseYear: string
+  title: string
+}
+
+interface PrimaryImage {
+  url: string
+}
+
+interface TitleText {
+  text: string
+}
+
+interface ReleaseYear {
+  year: string
+}
+
+export interface MoviesProps {
+  id: string
+  primaryImage: PrimaryImage
+  titleText: TitleText
+  releaseYear: ReleaseYear
+}
